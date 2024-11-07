e142436d4d2799a74a8b7965aa4be7b41fe5e117
Yash Mohod <mohodyash@gmail.com>
2024-11-07 12:04:54 -0500

    dimer update

--- mv.sh
+++ mv.sh
@@ -8,4 +8,13 @@
 # scp -r  ymohod@login.expanse.sdsc.edu:/expanse/lustre/projects/ith101/ymohod/E2-1-0.5j  /Users/mohod/Developer/otsim_data/E2-1-0.5j
 
 # scp -r  ymohod@login.expanse.sdsc.edu:/expanse/lustre/projects/ith101/ymohod/E2-var  /Users/mohod/Developer/OTsim/otsim_data/
-scp -r  ymohod@login.expanse.sdsc.edu:/expanse/lustre/projects/ith101/ymohod/E-2-var  /Users/mohod/Developer/OTsim/otsim_data/
+# scp -r  ymohod@login.expanse.sdsc.edu:/expanse/lustre/projects/ith101/ymohod/E-2-var  /Users/mohod/Developer/OTsim/otsim_data/
+
+
+# scp -r  ymohod@login.expanse.sdsc.edu:/expanse/lustre/projects/ith101/ymohod/D-1 /Users/mohod/Developer/OTsim/otsim_data/
+scp -r  ymohod@login.expanse.sdsc.edu:/expanse/lustre/projects/ith101/ymohod/D-2 /Users/mohod/Developer/OTsim/otsim_data/
+# scp -r  ymohod@login.expanse.sdsc.edu:/expanse/lustre/projects/ith101/ymohod/D-3 /Users/mohod/Developer/OTsim/otsim_data/
+# scp -r  ymohod@login.expanse.sdsc.edu:/expanse/lustre/projects/ith101/ymohod/D-4 /Users/mohod/Developer/OTsim/otsim_data/
+# scp -r  ymohod@login.expanse.sdsc.edu:/expanse/lustre/projects/ith101/ymohod/D-5 /Users/mohod/Developer/OTsim/otsim_data/
+
+scp -r  ymohod@login.expanse.sdsc.edu:/expanse/lustre/projects/ith101/ymohod/D2 /Users/mohod/Developer/OTsim/otsim_data/
